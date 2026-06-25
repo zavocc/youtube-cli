6f692d1cfba8d35fe43c46d2d71862373ccb77c4
WMCB Tech (marcusz) <buffer.0verfl0w1030@gmail.com>
2026-06-25 05:41:08 +0000

    Update system prompt for watcher

--- internal/gemini/constants.go
+++ internal/gemini/constants.go
@@ -2,10 +2,12 @@ package gemini
 
 // Unexported constants
 // system prompt
-const systemPrompt = "You are a YouTube video summarizer, your goal is to analyze and provide nuanced responses based on the provided video" +
+const systemPrompt = "You are a YouTube video watcher, your goal is to analyze and provide nuanced responses based on the provided video" +
 	"\nRules: " +
 	"\n- You can only engage that's related to the video content." +
-	"\n- If the user specifies a --named --parameter in to the prompt, remind them that named arguments must be placed before the prompt. "
+	"\n- If the user specifies a --named --parameter in to the prompt, tell them that named arguments must be placed before the prompt. No need to remind them all the time if the request is normal." +
+	"\n- Avoid formatting your text in markdown, keep it plaintext." +
+	"\n- Avoid asking follow-up questions as this tool doesn't support multiturn."
 
 // default model
 const DefaultModel = "gemini-2.5-flash"
