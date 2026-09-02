dfb212514512442507441f39801bab6ebc09e57e
zavocc <buffer.0verfl0w1030@gmail.com>
2026-09-02 21:15:59 +0800

    Increase output tokens

--- internal/gemini/client.go
+++ internal/gemini/client.go
@@ -88,7 +88,7 @@ func GApiClient(ctx context.Context, prompt string, url string, model string, re
 			MediaResolution:   genai.MediaResolution(mediaResLevel),
 			ResponseMIMEType:  "application/json",
 			ResponseSchema:    resSchema,
-			MaxOutputTokens:   8192,
+			MaxOutputTokens:   26000,
 			ServiceTier:       parsedServiceTier,
 		},
 	)
--- internal/gemini/constants.go
+++ internal/gemini/constants.go
@@ -6,8 +6,8 @@ const systemPrompt = "You are a YouTube video watcher, your goal is to analyze a
 	"\nRules: " +
 	"\n- You can only engage that's related to the video content." +
 	"\n- If the user specifies a --named --parameter in to the prompt, tell them that named arguments must be placed before the prompt. No need to remind them all the time if the request is normal." +
-	"\n- Avoid formatting your text in markdown and keep it plain." +
-	"\n- On the answer field, keep it direct, straight, keep it atleast 1-5 sentence descriptions of brief overview of the video, do not use markdown syntax." +
+	"\n- You must adapt the number 'evidence_timestamps' field based on how detailed the analysis needs to be or based on user's request, rather than always keeping it concise, for instance if it asked to list of exhaustive segments from the compilation video then it should be exhaustive." +
+	"\n- On the answer field, keep it direct, straight, keep it atleast 1-5 sentence descriptions brief overview of the video in 'answer' field, do not use markdown syntax." +
 	"\n- Avoid asking follow-up questions as this tool doesn't support multiturn." +
 	"\n- When timestampping the video, use the format DD:HH:MM:SS, and use proper conversion for hours, minutes, and seconds. For example, 48 hours would be 02:00:00:00 (or 2 days)."
 
